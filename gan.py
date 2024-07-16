72799e5ad18f439f7b4ffcbb349f1742f5a95f40
Damian Łącz <damianlacz@onet.pl>
2024-07-16 09:42:21 +0200

    Update gan.py

--- gan.py
+++ gan.py
@@ -10,31 +10,34 @@ class GAN(tf.keras.Model):
         self.binary_cross_entropy = BinaryCrossentropy(from_logits = True)
 
         self.gen_loss_weight = 1
-        self.real_loss_noise_weight = 0.1
-        self.real_loss_weight = 1024
-        self.fake_loss_noise_weight = 0.1
-        self.fake_loss_weight = 1024
+        self.real_loss_noise_weight = 0.2
+        self.real_loss_weight = 1
+        self.fake_loss_noise_weight = 0.2
+        self.fake_loss_weight = 1
 
         self.batch_size = kwargs.get('batch_size', 32)
-        self.gen_opt = tf.keras.optimizers.Adam(1e-4)
-        self.disc_opt = tf.keras.optimizers.Adam(2e-5)
+        self.gen_opt = tf.keras.optimizers.Adam(1e-3)
+        self.disc_opt = tf.keras.optimizers.Adam(1e-4)
 
     def compute_loss(self, inputs):
-        noise = tf.random.normal(shape = (self.batch_size, 512, 1))
+        noise = tf.random.normal(shape = (self.batch_size, 1024, ))
         fake_result = self.discriminator(self.generator(noise, training = True), training = True)
         real_result = self.discriminator(inputs, training = True)
 
-        real = tf.ones_like(real_result) - self.real_loss_noise_weight * tf.random.normal(shape = tf.shape(real_result))
-        fake = tf.zeros_like(fake_result) + self.fake_loss_noise_weight * tf.random.normal(shape = tf.shape(fake_result))
+        real = tf.ones_like(real_result) - self.real_loss_noise_weight * tf.random.uniform(shape = tf.shape(real_result), 
+                                                                                           minval = 0, maxval = 1)
+        fake = tf.zeros_like(fake_result) + self.fake_loss_noise_weight * tf.random.uniform(shape = tf.shape(fake_result), 
+                                                                                            minval = 0, maxval = 1)
 
         real_loss = self.real_loss_weight * self.binary_cross_entropy(real, real_result)
         fake_loss = self.fake_loss_weight * self.binary_cross_entropy(fake, fake_result)
 
-        gen_loss = self.gen_loss_weight * self.binary_cross_entropy(fake_result, tf.ones_like(fake_result))
+        gen_loss = self.gen_loss_weight * self.binary_cross_entropy(tf.ones_like(fake_result), fake_result)
         disc_loss = real_loss + fake_loss
 
         return gen_loss, disc_loss, real_loss, fake_loss
     
+    @tf.function
     def train_step(self, data):
         with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
             gen_loss, disc_loss, real_loss, fake_loss = self.compute_loss(data)
